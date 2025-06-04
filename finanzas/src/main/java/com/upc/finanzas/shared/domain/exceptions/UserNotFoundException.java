b8b063570bbc71fa6491e6a94fa251223017ba09
Piero Delgado <112528610+PieroD04@users.noreply.github.com>
2025-06-04 13:11:44 -0500

    feat: add shared components

---
+++ finanzas/src/main/java/com/upc/finanzas/shared/domain/exceptions/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package com.upc.finanzas.shared.domain.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException() {
+        super("User not found");
+    }
+
+    public UserNotFoundException(Long userId) {
+        super("User with id " + userId + " not found");
+    }
+}
